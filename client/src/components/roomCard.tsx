8d3b5707bd18bf90dbedc00a727dec11a754d4cf
Omar Saad <omarisadev@gmail.com>
2025-05-10 09:02:06 +0300

    card background updated for light theme

--- client/src/components/roomCard.tsx
+++ client/src/components/roomCard.tsx
@@ -10,7 +10,7 @@ interface props {
 
 function RoomCard({ room }: props) {
   return (
-    <Link className="bg-default-50 p-4 rounded-lg transition duration-400 shadow-2xl hover:shadow-primary-100" to={`/room/${room.id}`}>
+    <Link className="bg-foreground-100 dark:bg-default-50 p-4 rounded-lg transition duration-400 shadow-2xl hover:shadow-primary-100" to={`/room/${room.id}`}>
       <h3>{room.name}</h3>
       <small>{room.player}</small>
     </Link>
