c3fd4237b0638f87b11fb2a988df33c68d2171cc
Omar Saad <omarisadev@gmail.com>
2025-10-16 16:00:40 +0300

    feat: add vite-sitemap plugin and configuration for sitemap generation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import sitemap from "vite-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react(), tsconfigPaths()],
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      sitemap({
+        base: "https://ultimatexo.com/",
+        urls: ["instructions", "rooms", "create"],
+      }),
+    ],
     server: {
       proxy: {
         "/api": {
