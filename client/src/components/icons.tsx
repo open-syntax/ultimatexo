84aa0effc0712ce6d0055812fdbbcd6ac44952ba
Ahmed Mekkawi <68240398+ahmed-mekky@users.noreply.github.com>
2026-04-13 03:13:09 +0200

    improve chat ux

--- client/src/components/icons.tsx
+++ client/src/components/icons.tsx
@@ -268,6 +268,7 @@ export const X = ({ size = 24, ...props }: IconSvgProps) => (
     fill="none"
     width={size}
     height={size}
+    viewBox="0 0 24 24"
     stroke="currentColor"
     strokeLinecap="round"
     strokeLinejoin="round"
@@ -533,5 +534,23 @@ export const MessageCircle = ({ size = 24, ...props }: IconSvgProps) => (
     {...props}
   >
     <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
+  </svg>
+);
+
+export const Send = ({ size = 24, ...props }: IconSvgProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="m22 2-7 20-4-9-9-4z" />
+    <path d="M22 2 11 13" />
   </svg>
 );
